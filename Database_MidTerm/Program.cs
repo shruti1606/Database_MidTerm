50673bc8cedfbb319ab161c9872b0cb7bfd795a8
Admin <shrutip1606@gmail.com>
2024-06-27 15:13:27 -0400

    Add project files.

---
+++ Database_MidTerm/Program.cs
@@ -0,0 +1,7 @@
+﻿--Create Customers Table
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    total_spent DECIMAL(10,2),
+    created_at TIMESTAMP);
